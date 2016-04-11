2a0281e2158f56ba1193bc5538ad3ccb01078c95
phpAdways <ngoctd@ai-t.vn>
2016-04-11 14:19:10 +0700

    [mod] Add basic function: create, update post

--- lession5/controllers/AdminController.php
+++ lession5/controllers/AdminController.php
@@ -7,6 +7,21 @@ class AdminController extends Controller
 	 */
 	public function createAction() 
 	{
+		$message = '';
+
+		if (isset($_POST['title']) && isset($_POST['content'])) {
+			$title = $_POST['title'];
+			$content = $_POST['content'];
+			
+			if ($post = Node::create($title, $content)) {
+				header('Location:' . __());
+			} else {
+				$message = 'Add new post error';
+			}
+		}
+
+		$this->view->message = $message;
+
 		$this->render();
 	}
 
@@ -15,6 +30,28 @@ class AdminController extends Controller
 	 */
 	public function updateAction()
 	{
+		$id = isset($_GET['id']) ? $_GET['id'] : 0;
+		$post = Node::getPost($id);
+
+		$message = '';
+
+		if (isset($_POST['title']) && isset($_POST['content'])) {
+			$title = $_POST['title'];
+			$content = $_POST['content'];
+
+			if (Node::update($id, $title, $content)) {
+				$post->title = $title;
+				$post->content = $content;
+			} else {
+				$message = 'Update new error';
+			}
+		}
+
+		$this->view->id = $id;
+		$this->view->post = $post;
+		$this->view->message = $message;
+
 		$this->render();
 	}
+
 }
