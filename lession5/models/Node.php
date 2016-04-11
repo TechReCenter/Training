e8715d3a98276c654885eabb6345ddad9326c3a1
phpAdways <ngoctd@ai-t.vn>
2016-04-11 14:17:53 +0700

    [lession5] Add model method: update, create for table:Node

--- lession5/models/Node.php
+++ lession5/models/Node.php
@@ -13,7 +13,7 @@ class Node extends Model
 
     public static function getList($status = 1) 
     {
-        $stm = static::getCommand()->prepare('SELECT * FROM node WHERE status = :status');
+        $stm = static::getCommand()->prepare('SELECT * FROM node WHERE status = :status ORDER BY created DESC');
         $stm->execute(array('status' => $status));
 
         $nodes = array();
@@ -23,5 +23,38 @@ class Node extends Model
         }
 
         return $nodes;
+    }
+
+    public static function create($title, $content)
+    {
+        if (empty($title) || empty($content)) {
+            return false;
+        }
+
+        $command = static::getCommand();
+
+        $stm = $command->prepare('INSERT INTO node(title, content, status, created) VALUES(?, ?, ?, ?)');
+        $stm->execute(array($title, $content, 1, time()));
+
+        return $command->lastInsertId();
+    }
+
+    /**
+     * Update a row data
+     * @param  [type] $id      [description]
+     * @param  [type] $title   [description]
+     * @param  [type] $content [description]
+     * @return [type]          [description]
+     */
+    public static function update($id, $title, $content)
+    {
+        if (empty($id) || empty($title)) {
+            return false;
+        }
+
+        $stm = static::getCommand()->prepare('UPDATE node SET title=?, content=? WHERE id=?');
+        $stm->execute(array($title, $content, $id));
+
+        return $stm->rowCount();
     }
 }
