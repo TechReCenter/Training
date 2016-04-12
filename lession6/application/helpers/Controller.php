699f06a0fc8e39f28464aa29dc608b9f6d3b0d3c
phpAdways <ngoctd@ai-t.vn>
2016-04-12 18:09:11 +0700

    [lession6] Add view theme, view extension, rename view extension

--- lession6/application/helpers/Controller.php
+++ lession6/application/helpers/Controller.php
@@ -2,15 +2,32 @@
 
 class Controller extends CI_Controller
 {
+	/**
+	 * Store current view data
+	 * @var array
+	 */
 	var $view;
+
+	/**
+	 * View theme name
+	 * @var string
+	 */
+	var $theme;
 
 	public function __construct()
 	{
 		parent::__construct();
 
+		$this->theme = 'index';
 		$this->view = new ArrayObject([], ArrayObject::STD_PROP_LIST);
 	}
 
+	/**
+	 * Render view with theme
+	 * @param  string $view [description]
+	 * @param  array  $data [description]
+	 * @return [type]       [description]
+	 */
 	public function render($view = '', $data = [])
 	{
 		/**
@@ -20,8 +37,19 @@ class Controller extends CI_Controller
 			$view = $this->router->fetch_class() . '/' . $this->router->fetch_method();
 		}
 
+		/**
+		 * Build data put into view
+		 */
 		$data = (array) $data + (array) $this->view;
 
-		return $this->load->view($view, $data);
+		/**
+		 * Check for render with theme
+		 */
+		if ($this->theme) {
+			$data['content'] = $this->load->view($view . '.phtml', $data, true);
+			$this->load->view($this->theme . '.phtml', $data);
+		} else {
+			$this->load->view($view, $data);
+		}
 	}	
 }
