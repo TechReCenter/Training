8699fc1047a52097894ee7247f53b1c1b63c8340
phpAdways <ngoctd@ai-t.vn>
2016-04-12 16:51:49 +0700

    [lession6] Add bootstrap template for layout

--- lession6/application/views/post/detail.php
+++ lession6/application/views/post/detail.php
@@ -1,37 +1,128 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title><?php echo $title ?></title>
-  <link href="<?php echo __('assets/css/style.css') ?>" rel="stylesheet" type="text/css" />
-</head>
-<body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="//getbootstrap.com/favicon.ico">
 
-<div id="container">
-	<ul>
-	  <li><a href="<?php echo __() ?>">Home</a></li>
-	  <li><a href="<?php echo __('about') ?>">About</a></li>
-	  <li><a href="<?php echo __('admin/create') ?>">Create</a></li>
-	</ul>
+    <title><?php echo $title ?></title>
 
-	<hr>
+    <!-- Bootstrap core CSS -->
+    <link href="//getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
 
-	<h3><?php echo $node->title ?></a></h3>
-	<p><?php echo $node->content ?></p>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <link href="//getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
 
-	<hr>
+    <!-- Custom styles for this template -->
+    <link href="//getbootstrap.com/examples/blog/blog.css" rel="stylesheet">
 
-	<ul>
-	  <?php foreach($nodes as $k => $item) { ?>
-	  <?php if ($node->id == $k) continue ?>
-	  <li>
-	    <a href="<?php echo __($item->link) ?>"><?php echo $item->title ?></a>
-	  </li>
-	  <?php } ?>
-	</ul>
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="//getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
 
-  <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
-</div>
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
 
-</body>
+  <body>
+
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="<?php echo __() ?>">Home</a>
+          <a class="blog-nav-item" href="<?php echo __('news') ?>">New features</a>
+          <a class="blog-nav-item" href="<?php echo __('press') ?>">Press</a>
+          <a class="blog-nav-item" href="<?php echo __('hires') ?>">New hires</a>
+          <a class="blog-nav-item" href="<?php echo __('about') ?>">About</a>
+        </nav>
+      </div>
+    </div>
+
+    <div class="container">
+
+      <div class="blog-header">
+        <h1 class="blog-title">The Blog</h1>
+        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+      </div>
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main">
+          <div class="blog-post">
+            <h2 class="blog-post-title"><?php echo $node->title ?></h2>
+            <p class="blog-post-meta"><?php echo date('F n, Y', $node->created) ?> by <a href="#">Mark</a></p>
+            <p><?php echo $node->content ?></p>
+          </div><!-- /.blog-post -->
+
+          <ul>
+	  		<?php foreach($nodes as $k => $item) { ?>
+	  		<?php if ($node->id == $k) continue ?>
+	  		<li>
+	    		<a href="<?php echo __($item->link) ?>"><?php echo $item->title ?></a>
+	  		</li>
+	  		<?php } ?>
+			</ul>
+
+        </div><!-- /.blog-main -->
+
+        <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
+          <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </div>
+          <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">March 2014</a></li>
+              <li><a href="#">February 2014</a></li>
+              <li><a href="#">January 2014</a></li>
+              <li><a href="#">December 2013</a></li>
+              <li><a href="#">November 2013</a></li>
+              <li><a href="#">October 2013</a></li>
+              <li><a href="#">September 2013</a></li>
+              <li><a href="#">August 2013</a></li>
+              <li><a href="#">July 2013</a></li>
+              <li><a href="#">June 2013</a></li>
+              <li><a href="#">May 2013</a></li>
+              <li><a href="#">April 2013</a></li>
+            </ol>
+          </div>
+          <div class="sidebar-module">
+            <h4>Elsewhere</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">GitHub</a></li>
+              <li><a href="#">Twitter</a></li>
+              <li><a href="#">Facebook</a></li>
+            </ol>
+          </div>
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="//getbootstrap.com/assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="//getbootstrap.com/dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="//getbootstrap.com/assets/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
 </html>
